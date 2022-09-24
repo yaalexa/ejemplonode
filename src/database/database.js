8682cba77329865e68e94e5a8a3eff9a7ba9d219
alexagm12 <yaalexa25@hotmail.com>
2022-09-23 19:22:36 -0500

    first commit

--- src/database/database.js
+++ src/database/database.js
@@ -2,12 +2,10 @@ import mysql from "promise-mysql";
 import config from "./../config";
 
 const connection = mysql.createConnection({
-    //heroku_8f897e03cc97c6b?reconnect=true
-    
-    host: 'us-cdbr-east-06.cleardb.net',
-    database: 'heroku_8f897e03cc97c6b',
-    user: 'b05b6ecd87490f',
-    password: 'd6a6e0c8'
+    host: config.host,
+    database: 'prueba',
+    user: 'root',
+    password: ''
 });
 
 const getConnection = () => {
